bc75f910ef925e0b4bb442300847fe0efb8ea15f
Milos Pavlovic <milosar7@gmail.com>
2022-03-13 17:01:03 +0100

    More on strings

--- strings-and-arrays.ts
+++ strings-and-arrays.ts
@@ -6,3 +6,22 @@ function reverseString(str: string) {
 
 console.log(reverseString(""));
 console.log(reverseString("wolfs"));
+
+// 2. Check if string is palindromre
+function isPalindrome(str: string) {
+  if (str.length === 0 || str.trim().length === 0) return false;
+  return str.toLowerCase() === str.split("").reverse().join("").toLowerCase();
+}
+
+console.log(isPalindrome("Noou"));
+console.log(isPalindrome("Hannah"));
+
+// 3. Reverse an integer
+function reverseInteger(int: number) {
+  if (int < 10) return int;
+  const reversed = int.toString().split("").reverse().join("");
+  return Math.sign(int) * Number(reversed);
+}
+
+console.log(reverseInteger(2));
+console.log(reverseInteger(123));
